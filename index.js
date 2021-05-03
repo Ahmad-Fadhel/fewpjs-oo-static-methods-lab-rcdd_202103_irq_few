3329d27e6bd0220a4f62710167c0b3d385432965
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-03 11:15:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ class Formatter {
     return str.charAt(0).toUpperCase() + str.slice(1)
   }
   static sanitize(str){
-    return str.replace(/[^A-Za-z0-9-']+/g, '');
+    return str.replace(/[^A-Za-z0-9-" "']+/g, '');
   }
   static titleize(str){
     const exemptions = ["of", "the", "a", "an", "and", "but", "for", "at", "by", "from"]
 
-    return string.split(" ").map(function(word, index) { 
+    return str.split(" ").map(function(word, index) { 
       if (index !== 0 && exemptions.includes(word)) {
         return word
       } else {
